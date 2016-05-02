cb122189b23d2d90dd39fec4901749ee4486a828
bAshwal <b.ashwal@hotmail.com>
2016-05-02 18:44:34 -0500

    Update Mammal.java

--- Mammal.java
+++ Mammal.java
@@ -5,35 +5,33 @@
  */
 public class Mammal extends Animal
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public int NumberofArms;
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
+	public int NumberOfArms;
 	public int NumberOfLegs;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public Mammal(int Age, String Birth_method, String Consevervation_status, String Diet, boolean Flight, String Habit, boolean heathly, int height, String Last_Checkup, int Length, int Lifespan, String Location, String Name, int Weight){
+	public int getNumberOfArms()
+	{
+		return NumberOfArms;
+	}
+	public int getNumberOfLegs()
+	{
+		return NumberOfLegs;
+	}
+	
+	public Mammal(int Age, String Birth_method, String Consevervation_status, String Diet, boolean Flight, String Habit, boolean heathly, int height, String Last_Checkup, int Length, int Lifespan, String Location, String Name, int Weight, int NumberOfArms, int NumberOfLegs){
 		super(Age, Birth_method, Consevervation_status, Diet, Flight,  Habit, heathly, height, Last_Checkup, Length, Lifespan, Location, Name, Weight);
+		this.NumberOfArms = NumberOfArms;
+		this.NumberOfLegs = NumberOfLegs;
+		
 	}
 	
 	@Override
 	public String toString(){
-		return super.toString();
+		String info = super.toString();
+		info += "Number of Arms:" + getNumberOfArms() + "\n";
+		info += "Number of legs:" + getNumberOfLegs() + "\n";
+		
+		return info;
 	}
 }
 
