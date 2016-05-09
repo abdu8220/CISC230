7b96562fa5c9c51050ecfd11e221b1e6979b7a7f
Johnathan Marin <johnm7770@gmail.com>
2016-05-09 16:07:54 -0500

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -5,5 +5,26 @@
  * @since			5/4/16
  */
 public class Employee {
+	String address;
+	int hours;
+	int ID;
+	String name;
+	String phone;
+	double salary;
+	String SSN;
 	
+	public String	getAddress()	{ return address; }
+	public int 		getHours()	{ return hours; }
+	public int 		getID()			{ return ID;}
+	public String	getName()		{ return name; }
+	public String	getPhone()	{ return phone; }
+	public double	getSal()		{ return salary; }
+	public String	getSSN()		{ return SSN; }
+	
+	@Override
+	public String toString(){
+		String temp = "Infomation for: ";
+		temp += getName() + " employee type: " + this.getClass().getName() + "\n";
+		return temp;
+	}
 }
